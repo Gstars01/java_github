1bfd47fc95f6472d6be9841182014c0b2044a486
Gstars01 <khw4986@gmail.com>
2022-05-04 11:03:14 +0900

    First Commit

---
+++ calcul.java
@@ -0,0 +1,10 @@
+public class calcul {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 21;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+    }
+}
